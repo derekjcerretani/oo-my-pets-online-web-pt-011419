de474ec44dec954ffde65537bfea67beaa34141b
Derek Cerretani <derekcerretani@gmail.com>
2019-02-13 03:43:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,8 +66,6 @@ class Owner
   end
 
   def list_pets
-
-
     "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
     #binding.pry
   end
